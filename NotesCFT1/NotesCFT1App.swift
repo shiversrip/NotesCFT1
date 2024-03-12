3801ac3e32e053d342bda27043c0d2b9048fd411
shiversrip <patroffpatroff@gmail.com>
2024-03-12 16:34:10 +0300

    Initial Commit

---
+++ NotesCFT1/NotesCFT1App.swift
@@ -0,0 +1,17 @@
+//
+//  NotesCFT1App.swift
+//  NotesCFT1
+//
+//  Created by Андрей Елагин on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesCFT1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
